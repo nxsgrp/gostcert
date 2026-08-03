dce11c40406c988eda6ea8cb5c662096ebe5b49c
Gleb Akimov <gleakimov@ozon.ru>
2026-08-03 23:50:03 +0300

    chore: returned saving created cert

--- create_test.go
+++ create_test.go
@@ -3,6 +3,7 @@ package gostcert
 import (
 	"crypto/rand"
 	"math/big"
+	"os"
 	"testing"
 	"time"
 
@@ -12,6 +13,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	gost "github.com/tarantool/go-gostcrypto"
 	"github.com/tarantool/go-gostcrypto/x509gost"
+)
+
+const (
+	newTestCertPath = "test/resources/certs/created-certificate.der"
 )
 
 func TestCreateCertificate_SelfSigned(t *testing.T) {
@@ -56,8 +61,8 @@ func TestCreateCertificate_SelfSigned(t *testing.T) {
 	assert.NotEmpty(t, cert.cert.Raw, "expected cert raw bytes is not empty")
 	assert.NotEmpty(t, cert.cert.PubKeyRaw, "expected cert public key is not empty")
 	assert.NotEmpty(t, cert.cert.Stdlib.Subject.CommonName, "expected subject common name is not empty")
-	assert.Equal(t, cert.cert.GOSTAlgo, algorithm.AlgoR341012_256, "expected AlgoR341012_256 algorithm")
-	assert.Equal(t, cert.cert.SigGOSTAlgo, algorithm.AlgoR341012_256, "expected AlgoR341012_256 signature algorithm")
+	assert.Equal(t, cert.cert.GOSTAlgo, x509gost.AlgoR341012_256, "expected AlgoR341012_256 algorithm")
+	assert.Equal(t, cert.cert.SigGOSTAlgo, x509gost.AlgoR341012_256, "expected AlgoR341012_256 signature algorithm")
 
 	// Verify via x509gost
 	chains, err := cert.cert.Verify(x509gost.VerifyOptions{
@@ -67,8 +72,6 @@ func TestCreateCertificate_SelfSigned(t *testing.T) {
 	assert.NotEmpty(t, chains, "expected certificate chains is not empty")
 
 	// FIX: Remove after handle testing
-	//err = os.WriteFile("test.der", der, 0600)
-	//if err != nil {
-	//	t.Fatalf("WriteFile: %v", err)
-	//}
+	err = os.WriteFile(newTestCertPath, cert.cert.Raw, 0600)
+	assert.NoError(t, err, "failed to write new test certificate")
 }
