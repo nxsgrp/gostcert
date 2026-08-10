348d693c2e3865e6732a1b955cb4647c5f9dd022
Gleb Akimov <gleakimov@ozon.ru>
2026-08-10 09:16:00 +0300

    chore: update go doc for parse certificate func

--- parse.go
+++ parse.go
@@ -9,11 +9,6 @@ import (
 // ParseCertificate parses a DER-encoded X.509 certificate and returns a
 // GOST-aware Certificate wrapper.
 //
-// For GOST-signed certificates the returned Certificate will have GOST-specific
-// fields (IsGOST, GOSTAlgo, SigGOSTAlgo, PubKeyRaw) populated. Non-GOST
-// certificates are parsed through the stdlib and stored in the Stdlib field;
-// IsGOST will be false in that case.
-//
 // The function delegates to x509gost.ParseCertificate which handles both GOST
 // and non-GOST certificates transparently.
 func ParseCertificate(der []byte) (*Certificate, error) {
